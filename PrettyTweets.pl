e7629247de02a5c1d178de1196317986c97269cf
Dave Brown <davez1000@gmail.com>
2014-04-09 12:26:21 +0100

    Changed format of tweets output, and colours.

--- PrettyTweets.pl
+++ PrettyTweets.pl
@@ -9,37 +9,54 @@ $handle = sub {
 	$color = ${'CC'.scalar(&$tweettype($ref,&descape($ref->{'user'}->{'screen_name'}),$ref->{'text'}))};
 	$color = $OFF.$color;
 
+	my $usercolor = $CYAN;
+	my $menuselectcolor = $MAGENTA;
+	my $textatcolor = $RED;
+
 	my ($time, $ts) = &$wraptime($ref->{'created_at'}) if &getvariable('timestamp');
 	my $menuselect = '<'.$ref->{'menu_select'}.'> ';
 	my $timestamp = defined($ts)?'['.$ts.'] ':'';
 	my $names;
+
+	my $user_screen_name = &descape($ref->{'user'}->{'screen_name'});
+	if ($user_screen_name eq "Babette_Noodles") {
+		$usercolor = $BLUE;
+	}
+
 	if (defined $ref->{'user'}->{'name'}) {
-		$names = 
-			"${EM}".
+		$names =
+			"${EM}${usercolor}".
 			&descape($ref->{'user'}->{'name'}).
 			"$OFF".
-			' (http://twitter.com/'.
-			"${EM}".
-			&descape($ref->{'user'}->{'screen_name'}).
+			' ('.
+			"${EM}${usercolor}".
+			$user_screen_name .
 			"$OFF".
 			') '
 		;
 	} else {
-		$names = "${EM}".&descape($ref->{'user'}->{'screen_name'})."$OFF ";
+		$names = "${EM}".$user_screen_name."$OFF ";
 	}
-	my $text = "${color}".&descape($ref->{'text'})."$OFF";
+
+	my $tweet_text = &descape($ref->{'text'});
+	$color = $OFF.$YELLOW if ($tweet_text =~ /\@{$whoami}/);
+
+	my $text = "${color}".$tweet_text."$OFF";
 	my $bar = &descape("┃ ");
 	$text =~ s/\\n/\n$bar/g;
+
+	$text =~ s/(\@[\w_]+)/$textatcolor\1$OFF/g;
+	$text =~ s/(https?\S+)/$textatcolor\1$OFF/g;
 
 	print $streamout (
 		&descape("┏━━ "),
 		$names,
-		"${color}".$menuselect."$OFF",
+		"${menuselectcolor}".$menuselect."$OFF",
 		$timestamp,
 		"\n".&descape("┃ "),
 		#(' ' x length $menuselect),
 		$text,
-		"\n".&descape("┗━━")."\n",
+		"\n",
 	);
 	return 1;
 };
@@ -50,17 +67,20 @@ $dmhandle = sub {
 	$color = ${'CCdm'};
 	$color = $OFF.$color;
 
+	my $usercolor = $GREEN;
+	my $menuselectcolor = $MAGENTA;
+
 	my ($time, $ts) = &$wraptime($ref->{'created_at'}) if &getvariable('timestamp');
 	my $menuselect = '<DM d'.$ref->{'menu_select'}.'> ';
 	my $timestamp = defined($ts)?'['.$ts.'] ':'';
 	my $names;
 	if (defined $ref->{'sender'}->{'name'}) {
-		$names = 
-			"${EM}".
+		$names =
+			"${EM}${usercolor}".
 			&descape($ref->{'sender'}->{'name'}).
 			"$OFF".
-			' (http://twitter.com/'.
-			"${EM}".
+			' ('.
+			"${EM}${usercolor}".
 			&descape($ref->{'sender'}->{'screen_name'}).
 			"$OFF".
 			') '
@@ -75,12 +95,12 @@ $dmhandle = sub {
 	print $streamout (
 		&descape("┏━━ "),
 		$names,
-		"${color}".$menuselect."$OFF",
+		"${menuselectcolor}".$menuselect."$OFF",
 		$timestamp,
 		"\n".&descape("┃ "),
 		#(' ' x length $menuselect),
 		$text,
-		"\n".&descape("┗━━")."\n",
+		"\n",
 	);
 	return 1;
 };
